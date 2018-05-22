b95d90234d5c8d86bc31eaead65111c15d3ecbdc
Barış Ceviz <me@barisceviz.com>
2018-05-23 02:00:47 +0300

    Added Download Track Model to ClientApp

---
+++ SoundCloudDownloader/ClientApp/models/downloaderTrack.tsx
@@ -0,0 +1,15 @@
+﻿export default class DownloadTrackModel {
+    constructor(obj: any) {
+        this.title = obj.title;
+        this.playerEmbed = obj.playerEmbed;
+        this.imageUrl = obj.imageUrl;
+        this.previewUrl = obj.previewUrl;
+        this.downloadUrl = obj.downloadUrl;
+    }
+
+    title: string;
+    playerEmbed: string;
+    imageUrl: string;
+    previewUrl: string;
+    downloadUrl: string;
+}
