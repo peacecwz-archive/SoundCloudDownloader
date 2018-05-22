b3ff34517074938c18076ca04b5b66584f3d2c5c
Barış Ceviz <me@barisceviz.com>
2018-05-23 01:45:08 +0300

    Added Download track model

---
+++ SoundCloudDownloader/Models/DownloadTrackModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoundCloudDownloader.Models
+{
+    public class DownloadTrackModel
+    {
+        public string Title { get; set; }
+        public string PlayerEmbed { get; set; }
+        public string ImageUrl { get; set; }
+        public string PreviewUrl { get; set; }
+        public string DownloadUrl { get; set; }
+    }
+}
