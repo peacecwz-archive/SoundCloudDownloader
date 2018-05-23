a3717962a8bedb16b3c3c9807fcb283ef7d72896
Barış Ceviz <me@barisceviz.com>
2018-05-23 15:25:31 +0300

    Changed Home screen UI

--- SoundCloudDownloader/ClientApp/components/Home.tsx
+++ SoundCloudDownloader/ClientApp/components/Home.tsx
@@ -4,28 +4,45 @@ import { connect } from 'react-redux';
 import { ApplicationState } from '../store';
 import * as DownloaderStore from '../store/Downloader';
 
-type DownloaderProps =
-    DownloaderStore.DownloaderState       
-    & typeof DownloaderStore.actionCreators   
-    & RouteComponentProps<{ }>; 
+interface State {
+    trackUrl: string;
+}
 
-class DownloaderPage extends React.Component<DownloaderProps, {}> {
-    public componentWillMount() {
-        this.startToDownload();
+type DownloaderProps =
+    DownloaderStore.DownloaderState
+    & typeof DownloaderStore.actionCreators
+    & RouteComponentProps<{}>;
+
+class DownloaderPage extends React.Component<DownloaderProps, State> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            trackUrl: ''
+        };
     }
 
     private startToDownload() {
-        this.props.downloadMusic("https://soundcloud.com/peacecwz/skrillex-diplo-vs-florence-the-machine-where-are-u-now-vs-you-got-the-love-peacecwz-mashup");
+        this.props.downloadMusic(this.state.trackUrl);
     }
 
     public render() {
-        return <div>
-            {this.props.isLoading ? <p>Loading...</p> : <p>{this.props.downloadUrl}</p>}
-        </div>;
+        return (
+            <div className="main-content-container">
+                <input autoFocus type="text" onChange={e => this.setState({
+                    trackUrl: e.target.value
+                })} />
+                <button onClick={this.startToDownload.bind(this)}>Convert</button>
+                {this.props.isLoading ? (
+                    <div className="results popup">
+                        <span className='popuptext' id='message'>Message</span>
+                        <div className='loader' id='loading'></div>
+                    </div>) : null}
+            </div>
+        );
     }
 }
 
 export default connect(
-    (state: ApplicationState) => state.downloader, 
-    DownloaderStore.actionCreators                
+    (state: ApplicationState) => state.downloader,
+    DownloaderStore.actionCreators
 )(DownloaderPage) as typeof DownloaderPage;
